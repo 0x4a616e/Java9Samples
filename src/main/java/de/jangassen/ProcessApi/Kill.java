689de806f9159c70a3d63fce7a39b8c2ab80413e
Jan Gassen <jan.gassen@centerdevice.com>
2016-05-31 17:23:59 +0200

    Add kill example

---
+++ src/main/java/de/jangassen/ProcessApi/Kill.java
@@ -0,0 +1,15 @@
+package de.jangassen.ProcessApi;
+
+/**
+ * Created by jan on 31/05/16.
+ */
+public class Kill {
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.err.println("Usage: " + ProcessHandle.current().info().commandLine().orElse("<kill>") + " <pid>");
+      return;
+    }
+
+    ProcessHandle.of(Long.parseLong(args[0])).ifPresent(handle -> handle.destroy());
+  }
+}
